56e588d93be5c0dc01fe590baea4fe70b2a43271
Darya Tatarinova <tatarinova0903@yandex.ru>
2025-01-03 13:35:38 +0300

    empty ios app

---
+++ ImagePredictor/ImagePredictor/ImagePredictorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagePredictorApp.swift
+//  ImagePredictor
+//
+//  Created by Daria Tatarinova on 03.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ImagePredictorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
